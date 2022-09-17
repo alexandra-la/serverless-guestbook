b15802875b0f168c7bd1ce34061706ea03424241
alexandra-la <106029681+alexandra-la@users.noreply.github.com>
2022-09-17 11:43:41 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/549dc471-d094-4de0-a396-cc90e712e6da/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${/read-guestbook-entries-sequence.json}/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${/save-guestbook-entry-sequence.json}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
